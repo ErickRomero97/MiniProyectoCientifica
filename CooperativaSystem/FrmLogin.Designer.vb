7ae9ed8a0aeba87110d8eada8bcc8ba658d1effe
Erick Romero <romero.gallardo97@gmail.com>
2017-04-14 22:30:33 -0600

    Splash

---
+++ CooperativaSystem/FrmLogin.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmLogin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.BtnSalir = New System.Windows.Forms.Button()
+        Me.BtnIngresar = New System.Windows.Forms.Button()
+        Me.TxtPassword = New System.Windows.Forms.TextBox()
+        Me.TxtUser = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'BtnSalir
+        '
+        Me.BtnSalir.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnSalir.Location = New System.Drawing.Point(336, 127)
+        Me.BtnSalir.Name = "BtnSalir"
+        Me.BtnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.BtnSalir.TabIndex = 8
+        Me.BtnSalir.Text = "Salir"
+        Me.BtnSalir.UseVisualStyleBackColor = True
+        '
+        'BtnIngresar
+        '
+        Me.BtnIngresar.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnIngresar.Location = New System.Drawing.Point(336, 88)
+        Me.BtnIngresar.Name = "BtnIngresar"
+        Me.BtnIngresar.Size = New System.Drawing.Size(75, 23)
+        Me.BtnIngresar.TabIndex = 7
+        Me.BtnIngresar.Text = "Ingresar"
+        Me.BtnIngresar.UseVisualStyleBackColor = True
+        '
+        'TxtPassword
+        '
+        Me.TxtPassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtPassword.ForeColor = System.Drawing.Color.Black
+        Me.TxtPassword.Location = New System.Drawing.Point(101, 125)
+        Me.TxtPassword.Multiline = True
+        Me.TxtPassword.Name = "TxtPassword"
+        Me.TxtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.TxtPassword.Size = New System.Drawing.Size(173, 27)
+        Me.TxtPassword.TabIndex = 6
+        '
+        'TxtUser
+        '
+        Me.TxtUser.BackColor = System.Drawing.SystemColors.Window
+        Me.TxtUser.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtUser.ForeColor = System.Drawing.Color.Black
+        Me.TxtUser.Location = New System.Drawing.Point(101, 86)
+        Me.TxtUser.Multiline = True
+        Me.TxtUser.Name = "TxtUser"
+        Me.TxtUser.Size = New System.Drawing.Size(173, 27)
+        Me.TxtUser.TabIndex = 5
+        Me.TxtUser.Tag = ""
+        '
+        'FrmLogin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(513, 243)
+        Me.Controls.Add(Me.BtnSalir)
+        Me.Controls.Add(Me.BtnIngresar)
+        Me.Controls.Add(Me.TxtPassword)
+        Me.Controls.Add(Me.TxtUser)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "FrmLogin"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FrmLogin"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnSalir As Button
+    Friend WithEvents BtnIngresar As Button
+    Friend WithEvents TxtPassword As TextBox
+    Friend WithEvents TxtUser As TextBox
+End Class
---
+++ CooperativaSystem/FrmLogin.vb
@@ -0,0 +1,37 @@
+﻿Imports System.Data.SqlClient
+Public Class FrmLogin
+    Private Sub BtnIngresar_Click(sender As Object, e As EventArgs) Handles BtnIngresar.Click
+        If cnn.State = ConnectionState.Open Then
+            cnn.Close()
+        End If
+        Try
+            cnn.Open()
+            Using cmd As New SqlCommand
+
+                With cmd
+                    .CommandText = "Sp_Login"
+                    .CommandType = CommandType.StoredProcedure
+                    .Connection = cnn
+                    .Parameters.Add("@UserName", SqlDbType.NVarChar).Value = TxtUser.Text
+                    .Parameters.Add("@pass", SqlDbType.NVarChar).Value = TxtPassword.Text
+                End With
+                Dim reader As SqlDataReader = cmd.ExecuteReader
+
+                If reader.HasRows Then
+                    reader.Read()
+                    FrmPrincipal.Show()
+                    Me.Close()
+                Else
+                    MsgBox("Usuario y Contrasena invalido")
+                End If
+            End Using
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles BtnSalir.Click
+        Me.Close()
+    End Sub
+End Class
